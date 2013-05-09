e37dbcc87e9bcb7d166c00715b1c86423f08fcff
David Nordlund <github@tangledweb.info>
2013-05-09 17:20:01 -0300

    Add video functionality.

--- atom.xsl
+++ atom.xsl
@@ -4,29 +4,55 @@
                xmlns:atom="http://www.w3.org/2005/Atom"
                xmlns:f="http://novascotia.ca/features/v1"
                exclude-result-prefixes="atom f"
-><xsl:output method="xml" omit-xml-declaration="yes" indent="yes"/>
+><xsl:output method="xml" omit-xml-declaration="yes" indent="no"/>
 <xsl:strip-space elements="*"/>
 
 <xsl:template match="atom:feed">
-	<div class="features">
+	<xsl:variable name="howmany" select="count(atom:entry)"/>
+	<xsl:comment>features: <xsl:value-of select="atom:link[@rel='self']/@href"/> × atom.xsl = </xsl:comment>
+	<div class="features" data-analytics="category={atom:title}">
+		<xsl:if test="translate(f:duration/@autostart,'FALSE','false')='false'"><xsl:attribute name="data-autostart">0</xsl:attribute></xsl:if>
 		<xsl:if test="f:duration/@seconds"><xsl:attribute name="data-duration"><xsl:value-of select="f:duration/@seconds"/></xsl:attribute></xsl:if>
 		<xsl:apply-templates select="atom:entry"/>
-		<xsl:comment>⁠<xsl:value-of select="count(atom:entry)"/> feature(s)</xsl:comment>
+		<xsl:comment><xsl:value-of select="$howmany"/> feature(s)</xsl:comment>
+		<xsl:if test="$howmany &gt; 1">
+		<div class="featureControls" aria-hidden="true">
+			<xsl:apply-templates select="." mode="index"/>
+			<button class="featureBackBtn"> </button>
+			<button class="featureNextBtn"> </button>
+		</div>
+		</xsl:if>
 	</div>
+	<xsl:comment>/features</xsl:comment>
 </xsl:template>
 
 <xsl:template match="atom:entry">
-	<xsl:variable name="duration" select="f:duration/@seconds"/>
-	<xsl:variable name="defaulthref"><xsl:call-template name="getdefaulthref"/></xsl:variable>
+	<xsl:variable name="mainlink" select="atom:link[not(@rel) or @rel='alternate']"/>
+	<xsl:variable name="playable" select="atom:link[@f:linktype='video']"/>
+	<xsl:variable name="playtype" select="$playable/@f:linktype"/>
+	<xsl:variable name="playfullhost" select="substring-before(substring-after($playable/@href, '//'), '/')"/>
+	<xsl:variable name="playhost"><xsl:choose><xsl:when test="starts-with($playfullhost, 'www.')"><xsl:value-of select="substring-after($playfullhost, 'www.')"/></xsl:when><xsl:otherwise><xsl:value-of select="$playfullhost"/></xsl:otherwise></xsl:choose></xsl:variable>
+	<xsl:variable name="photohref"><xsl:choose><xsl:when test="$playable"><xsl:value-of select="$playable/@href"/></xsl:when><xsl:otherwise><xsl:value-of select="$mainlink/@href"/></xsl:otherwise></xsl:choose></xsl:variable>
 	<xsl:variable name="datetime" select="atom:published"/>
 	<xsl:variable name="pubdate" select="substring($datetime, 1, 10)"/>
 	<xsl:variable name="photo" select="atom:link[@rel='enclosure']"/>
-	<article class="feature">
-		<xsl:if test="$duration"><xsl:attribute name="data-duration"><xsl:value-of select="$duration"/></xsl:attribute></xsl:if>
-		<a class="featurePhoto" href="{$defaulthref}"><img src="{$photo/@href}" alt="{$photo/@title}" title="{$photo/@f:cutline}" /></a>
+	<xsl:variable name="label" select="substring-after(atom:id, '#')"/>
+	<xsl:variable name="value" select="count(preceding-sibling::atom:entry)+1"/>
+	<xsl:variable name="action"><xsl:choose><xsl:when test="$playable">Played <xsl:value-of select="$playtype"/></xsl:when><xsl:otherwise>Clicked photo</xsl:otherwise></xsl:choose></xsl:variable>
+	<div class="feature" data-analytics="label={$label};value={$value}">
+		<a class="featurePhoto" href="{$photohref}" data-analytics="action={$action}"
+		 ><img src="{$photo/@href}" alt="{$photo/@title}" title="{$photo/@f:cutline}"
+		 /><xsl:if test="$playable"><span class="featurePlayBtn" title="{$playable/@title}"> </span></xsl:if
+		></a>
+		<xsl:if test="$playable">
+		<div class="featurePlayer"><button class="featurePlayerClose" title="Close {$playtype}">X</button></div>
+		</xsl:if>
 		<div class="featureOverlay">
 			<div class="featureContent">
-				<h1 class="featureTitle" title="{atom:title} ({$pubdate})"><a href="{$defaulthref}"><xsl:value-of select="atom:title"/></a></h1>
+				<h1 class="featureTitle" title="{atom:title} ({$pubdate})"
+				><a href="{$mainlink/@href}" data-analytics="action=Clicked headline"
+				><xsl:value-of select="atom:title"
+				/></a></h1>
 				<time pubdate="pubdate" datetime="{$datetime}" class="featurePublished"><xsl:value-of select="$pubdate"/></time>
 				<div class="featureSummary">
 					<p><xsl:value-of select="atom:summary"/></p>
@@ -35,27 +61,29 @@
 					<xsl:apply-templates select="atom:link[not(@rel='enclosure')]"/>
 				</ul>
 			</div>
-			<div class="featureBack"></div>
-			<div class="featureNext"></div>
+			<div class="featureBack"> </div>
+			<div class="featureNext"> </div>
 		</div>
-	</article>
+	</div>
 </xsl:template>
-
-<xsl:template name="getdefaulthref"
-><xsl:value-of select="atom:link[not(@rel) or @rel='alternate']/@href"/></xsl:template>
-<!--xsl:for-each select="f:links/*[position()=1]"><xsl:choose>
-	<xsl:when test="@href"><xsl:value-of select="@href"/></xsl:when>
-	<xsl:otherwise><xsl:apply-templates select="." mode="href"/></xsl:otherwise>
-</xsl:choose></xsl:for-each
-></xsl:template-->
 
 <xsl:template match="atom:link">
 	<xsl:variable name="text"><xsl:choose><xsl:when test="@title"><xsl:value-of select="@title"/></xsl:when><xsl:otherwise><xsl:apply-templates select="." mode="text"/></xsl:otherwise></xsl:choose></xsl:variable>
 	<xsl:variable name="href"><xsl:choose><xsl:when test="@href"><xsl:value-of select="@href"/></xsl:when><xsl:otherwise><xsl:apply-templates select="." mode="href"/></xsl:otherwise></xsl:choose></xsl:variable>
 	<xsl:variable name="lang"><xsl:choose><xsl:when test="@xml:lang"><xsl:value-of select="@xml:lang"/></xsl:when><xsl:otherwise>en</xsl:otherwise></xsl:choose></xsl:variable>
 	<li class="featureLinkItem {@f:linktype}FeatureLink">
-		<a href="{@href}" class="featureLink" hreflang="{@hreflang}" lang="{$lang}"><xsl:value-of select="@title"/></a>
+		<a href="{@href}" class="featureLink" hreflang="{@hreflang}" lang="{$lang}"
+		   data-analytics="action=Clicked {@f:linktype} ({$lang})"
+		 ><xsl:value-of select="@title"/></a>
 	</li>
+</xsl:template>
+
+<xsl:template match="atom:feed" mode="index">
+	<ol class="featuresIndex">
+		<xsl:for-each select="atom:entry">
+		<li><button title="{atom:title} ({substring(atom:published,1,10)})">•</button></li>
+		</xsl:for-each>
+	</ol>
 </xsl:template>
 
 </xsl:transform>
--- features.js
+++ features.js
@@ -4,7 +4,7 @@ var Features = {
 		Features.transitioner = Features.Transitioners.init();
 		var boxes = Features.byClass.init().get(document.body, 'features');
 		Features.box = new Array(boxes.length);
-		for (var i=Features.box.length; i--; Features.box[i] = new Features.Box(boxes[i], i));
+		for (var i=Features.box.length; i--; Features.box[i]=new Features.Box(boxes[i], i));
 	},
 	byClass: { // get elements in the DOM by classname
 		init: function() { (document.getElementsByClassName) || (this.get = this.find); return this; },
@@ -17,68 +17,55 @@ var Features = {
 			return found;
 		}
 	},
-	Box: function(tag, i) { this.init(tag, i); }
+	Box: function(tag, i) { this.init(tag, i, Features.byClass); },
+	LOCKS: {FREEZE:1,FOCUS:2,HOVER:4,TRANS:8,PLAYER:16}
 }
 
 Features.Box.prototype = {
 	idx: 0,	// index of current feature
 	timer: 0,
-	busy: false,
-	init: function(tag, boxId) {
-		var list = Features.byClass.get(tag, 'feature');
-		this.length = list.length;
-		if (this.length < 2)	// only setup bells & whistles if box has multiple features
-			return;
+	init: function(tag, boxId, $) {
+		var list = $.get(tag, 'feature'), btns = $.get(tag, 'featureControls');
+		this.size = list.length;
+		if (!this.size) return;
 		this.tag = tag;
-		var jslink = 'javascript:Features.box['+boxId+'].';
-		var defaultduration = tag.getAttribute('defaultduration') || 9;
-		var dots = document.createElement('ol');
-		dots.setAttribute("aria-hidden", "true");
-		this.createCallbacks(this);
-		this.feature = new Array(this.length);
-		for (var i=0, f=list[0]; i < this.length; f=list[++i]) {
-			this.feature[i] = {
-				tag: f,
-				dot: dots.appendChild(document.createElement('li')).appendChild(document.createElement('a')),
-				photo: Features.byClass.get(f, 'featurePhoto')[0],
-				duration: (f.getAttribute('duration') || defaultduration) * 1000
-			}
-			this.feature[i].dot.onfocus = this.wait;
-			for (var a=f.getElementsByTagName('a'), j=a.length; j--; a[j].onfocus=this.focused, a[j].onblur=this.blurred);
-			i && (f.className+=' hiddenFeature');
+		this.createCallbacks($);
+		this.story = new Array(this.size);
+		this.duration = (tag.getAttribute('data-duration')||9)*1000;
+		btns = btns.length?btns[0].getElementsByTagName('button'):[];
+		for (var i=0,t=list[0]; i<this.size; t=list[++i]) {
+			var p=$.get(t, 'featurePhoto')[0], b=$.get(p, 'featurePlayBtn');
+			this.story[i] = {tag: t, pic: p, dot: i in btns?btns[i]:{}};
+			b.length && (p.onclick = this.play);
+			if (this.size>1)
+				for (var a=t.getElementsByTagName('a'), j=a.length; j--; a[j].onfocus=this.linkFocus, a[j].onblur=this.linkBlur);
+			i && (t.className+=' hiddenFeature');
 		}
-		for (var btn=[{t:'Back',f:'back'}, {t:'Next',f:'next'}], i=2, b; i && (b=btn[--i]); a.href=jslink+b.f+'();') {
-			var a = document.createElement('a');
-			a.className = 'feature'+b.t+'Btn';
-			a.setAttribute("aria-hidden", "true");
-			a.appendChild(document.createTextNode(b.t));
-			tag.appendChild(a).onfocus = this.wait;
-		}
-		var bullet = "•"; bullet.length==1 || (bullet = '*');
-		for (var i=this.length, a, t; i-- && (a=this.feature[i].dot); a.href=jslink+'jump('+i+')') {
-			a.appendChild(document.createTextNode(bullet));
-			t = Features.byClass.get(this.feature[i].tag,'featureTitle');
-			if (t.length) a.setAttribute("title", t[0].getAttribute("title"));
-		}
-		tag.appendChild(dots).className = 'featuresIndex';
-		this.feature[0].dot.className = 'featureIndex';
+		for (var i=btns.length, b; i-- && (b=btns[i]); b.onfocus=this.btnFocus, b.onblur=this.btnBlur)
+			b.onclick = b.className.length ? this[b.className.substring(7,11).toLowerCase()] : this.jump;
+		this.story[0].dot.className = 'featureIndex';
 		tag.className += ' jsFeatures';
-		tag.onmouseover = this.wait;
-		this.wait();
+		tag.getAttribute('data-autostart')=='0' ? this.lock(Features.LOCKS.FREEZE) : this.wait();
 	},
-	next: function() { var i=this.getNext(); this.transition(i?"Next":"Wrap", i); },
-	back: function() { this.transition(this.idx?"Back":"WrapBack", this.getBack()); },
-	jump: function(i) { this.transition("Jump", i); },
-	createCallbacks: function(box) {
+	locked: 0, //locked boxes don't transition
+	lock: function(key) {(this.locked|= key) && this.stop()},
+	unlock: function(key) {(this.locked&=~key) || this.wait()},
+	createCallbacks: function($) {
+		var LOCK=Features.LOCKS, box=this;
+		box.next = function() { var i=box.getNext(); box.transition(i?"Next":"Wrap", i); };
+		box.back = function() { box.transition(box.idx?"Back":"WrapBack", box.getBack()); };
 		box.auto = function() { box.timer=0; var i=box.getNext(); box.transition(i?"Auto":"Cycle", i); };
+		box.jump = function() { for (var i=box.size; i-- && (box.story[i].dot!=this);); box.transition("Jump",i); };
 		box.trans = {
 			old: 0, next: 0, name: '',
 			set: function(transname, old_idx, next_idx) {
-				if (!box.busy) {
-					box.timer && window.clearTimeout(box.timer);
-					this.old=box.feature[old_idx], this.next=box.feature[next_idx], this.name=transname;
+				if (!this.name) {
+					box.lock(LOCK.TRANS);
+					box.unlock(LOCK.FREEZE);
+					this.old=box.story[old_idx], this.next=box.story[next_idx], this.name=transname;
+					(box.locked&LOCK.PLAYER) && box.delPlayer(this.old);
 					Features.transitioner.set(box, this);
-					return (box.busy = true);
+					return true;
 				}
 				return false;
 			},
@@ -86,32 +73,63 @@ Features.Box.prototype = {
 			end: function() {
 				box.tag.className = 'features jsFeatures';
 				(box.trans.next!=box.trans.old) && (box.trans.old.tag.className = 'feature hiddenFeature');
-				box.busy = false;
-				box.wait();
+				box.trans.name = '';
+				box.unlock(LOCK.TRANS);
 			}
-		};
-		box.wait = function() {
-			box.timer && window.clearTimeout(box.timer);
-			var d=box.feature[box.idx].duration;
-			box.timer = d && window.setTimeout(box.auto, Math.max(d, 1000)) || 0;
 		}
-		box.focused = function() {
-			var i=box.length, f=this;
+		box.stop = function() {box.timer && (box.timer=clearTimeout(box.timer))}
+		box.wait = function() {box.stop(); var d=box.duration; d && (box.timer=setTimeout(box.auto, d))}
+		box.linkFocus = function() {
+			var i=box.size, f=this;
 			for (; f && !f.className.match(/^feature\b/); f=f.parentNode)
 				if (f.className=="featureLinks") f.className += ' featureLinksFocused';
-			while((i--) && (box.feature[i].tag!=f)); // set i = feature # with focused link
-			(i!=box.idx) && box.jump(i) || box.wait();
-		},
-		box.blurred = function() {(this.className=="featureLink")&&(this.parentNode.parentNode.className='featureLinks');}
+			while((i--) && (box.story[i].tag!=f)); // set i = feature # with focused link
+			(i!=box.idx) && box.transition("Jump",i);
+			box.lock(LOCK.FOCUS);
+		}
+		box.linkBlur = function() {
+			(this.className=="featureLink")&&(this.parentNode.parentNode.className='featureLinks');
+			box.unlock(LOCK.FOCUS);
+		}
+		box.btnFocus = function() {box.lock(LOCK.FOCUS)}
+		box.btnBlur = function() {box.unlock(LOCK.FOCUS)}
+		box.tag.onmouseover = function() {box.lock(LOCK.HOVER)}
+		box.tag.onmouseout  = function() {box.unlock(LOCK.HOVER)}
+		box.play = function() {
+			var url = this.href, src, story=box.story[box.idx].tag,
+				youtube=url.match(/http:\/\/(www\.)?youtube.com\/watch\?v=([^&]+)/);
+			if (box.locked&(LOCK.TRANS|LOCK.PLAYER)) return false;
+			if (youtube) src='www.youtube-nocookie.com/embed/'+youtube[2]+'?autoplay=1&rel=0&autohide=1';
+			if (!src) return true;
+			box.lock(LOCK.PLAYER);
+			var i=document.createElement('iframe'), p=$.get(story, 'featurePlayer')[0], c=$.get(story, 'featurePlayerClose')[0];
+			story.className+=' featurePlaying';
+			c.onclick=box.noplay;
+			c.focus();
+			i.className='featurePlayerObject';
+			p.appendChild(i).setAttribute('src', location.protocol+'//'+src);
+			i.style.height = p.clientHeight+'px';
+			return false;
+		}
+		box.noplay = function() {
+			var f=box.story[box.idx];
+			box.delPlayer(f);
+			f.pic.focus();
+		}
 	},
-	getNext: function() { return (this.idx + 1) % this.length; },
-	getBack: function() { return (this.idx + this.length - 1) % this.length; },
+	getNext: function() { return (this.idx + 1) % this.size; },
+	getBack: function() { return (this.idx + this.size - 1) % this.size; },
 	transition: function(transname, idx) {
 		if (this.trans.set(transname, this.idx, idx)) {
-			this.feature[this.idx].dot.className = '';
-			this.feature[idx].dot.className = 'featureIndex';
+			this.story[this.idx].dot.className = '';
+			this.story[idx].dot.className = 'featureIndex';
 			this.idx = idx;
 		}
+	},
+	delPlayer: function(story) {
+		for (var l=Features.byClass.get(story.tag, 'featurePlayerObject'),i=l.length,o; i-- && (o=l[i]); o.parentNode.removeChild(o));
+		story.tag.className = 'feature';
+		this.unlock(Features.LOCKS.PLAYER);
 	}
 }
 
@@ -127,13 +145,13 @@ Features.Transitioners = {
 		set: function(box, t) {
 			box.tag.className = 'features jsFeatures featuresTransition'+t.name;
 			t.next.tag.className = 'feature featureTransitionIn feature'+t.name+'In';
-			window.setTimeout(t.go, 30);
+			setTimeout(t.go, 30);
 		},
 		go: function(box, t) {
 			box.tag.className += ' featuresTransition';
 			t.old.tag.className = 'feature featureTransitionOut feature'+t.name+'Out';
 			t.next.tag.className = 'feature';
-			window.setTimeout(t.end, 1001);
+			setTimeout(t.end, 1001);
 		}
 	},
 	StopMotion: {	// for old browsers without Transitions
@@ -143,7 +161,7 @@ Features.Transitioners = {
 			return this;
 		},
 		set: function(box, t) {
-			var p = t.next.photo.style;
+			var p = t.next.pic.style;
 			p.left = p.top = 0;
 			switch (t.name) {
 				case "Back": case "WrapBack": p.left = '-100%'; break;
@@ -152,26 +170,26 @@ Features.Transitioners = {
 			}
 			t.next.tag.className = 'feature';
 			t.remaining = this.stops;
-			t.interval = window.setInterval(t.go, Math.floor(800 / this.stops));
+			t.interval = setInterval(t.go, Math.floor(800 / this.stops));
 		},
 		go: function(box, t) {
-			var i = --t.remaining, oldphoto = t.old.photo.style, newphoto = t.next.photo.style;
+			var i = --t.remaining, oldpic = t.old.pic.style, newpic = t.next.pic.style;
 			switch (t.name) {
 				case "Back": case "WrapBack":
-					oldphoto.left = (100 - this.curve[i]) + '%';
-					newphoto.left = (-this.curve[i]) + '%';
+					oldpic.left = (100 - this.curve[i]) + '%';
+					newpic.left = (-this.curve[i]) + '%';
 					break;
 				case "Jump":
-					oldphoto.top  = (this.curve[i] - 100) + '%';
-					newphoto.top  = 0.8 * this.curve[i] + '%';
+					oldpic.top  = (this.curve[i] - 100) + '%';
+					newpic.top  = 0.8 * this.curve[i] + '%';
 					break;
 				default:
-					oldphoto.left = (this.curve[i] - 100) + '%';
-					newphoto.left = this.curve[i] + '%';
+					oldpic.left = (this.curve[i] - 100) + '%';
+					newpic.left = this.curve[i] + '%';
 					break;
 			}
 			if (!t.remaining) {
-				window.clearInterval(t.interval);
+				clearInterval(t.interval);
 				t.end();
 			}
 		}
